5ecb46cab69d6d3d03e0648d64775e9dbf44fa40
Depri0n <Depri0n@yandex.ru>
2020-10-21 15:05:26 +0300

    Laba4, Laba5

---
+++ Laba4/Laba4.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class Device
+{
+private:
+	string name, manufacturer;
+	int power;
+public:
+	void InputPower(int _power) { power = _power; };
+	void OutputInfo()
+	{
+		cout << "name:" << name << endl << "power:" << power << endl << "manufacturer:" << manufacturer << endl;
+	}
+	Device(string _name, int _power, string _manufacturer) { name = _name, power = _power, manufacturer = _manufacturer; }
+	~Device() {};
+};
+
+class Sound
+{
+private:
+	int frequency, volume;
+public:
+	void InputVolume(int _volume) { volume = _volume; };
+	void OutputInfo(){ cout << "frequency:" << frequency << endl << "volume:" << volume << endl; }
+	Sound(int _frequency, int _volume) { frequency = _frequency, volume = _volume; }
+	~Sound() {};
+};
+
+class Siren : Device, Sound
+{
+public:
+	Siren(string _name, int _power, string _manufacturer, int _frequency, int _volume) :
+		Device(_name, _power, _manufacturer), Sound(_frequency, _volume) {};
+};
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Device:" << endl;
+	Device* dev = new Device("Rock", 10, "Germany");
+	dev->OutputInfo();
+	cout << "Sound:" << endl;
+	Sound* sound = new Sound(144, 8);
+	sound->OutputInfo();
+	cout << "Siren:" << endl;
+	Siren* siren = new Siren("Rock", 10, "Germany", 144, 8);
+	((Device*)siren)->Device::OutputInfo();
+	((Sound*)siren)->Sound::OutputInfo();
+	delete dev;
+	delete sound;
+	delete siren;
+}
---
+++ Laba5/Laba5.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+class Matrix;
+
+class Vector
+{
+private:
+    int v[5];
+public:
+    void Output() { cout << v << endl; }
+    void OutputS()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << v[i];
+        }
+    }
+    Vector() {}
+    Vector(int _v[]) 
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            v[i] = _v[i];
+        }
+    }
+    ~Vector() {};
+
+    friend int Func(Vector v1, Matrix m1);
+};
+
+class Matrix
+{
+private:
+    int m[5][2];
+public:
+    void Output() { cout << m << endl; }
+    void OutputS() 
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            cout << m[i][i];
+        }
+    }
+    Matrix() {}
+    Matrix(int _m[5][2]) 
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                m[i][j] = _m[i][j];
+            }
+        }
+    }
+    ~Matrix() {};
+
+    friend int Func(Vector v1, Matrix m1);
+};
+
+int Func(Vector v1, Matrix m1)
+{
+    int maxV = 1;
+    int maxM = 1;
+    for (int i = 0; i < 5; i++)
+    {
+        if (maxV < (int)v1.v[i]) maxV = (int)v1.v[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            if (maxM < (int)m1.m[i][j]) maxM = (int)m1.m[i][j];
+        }
+    }
+    return maxV + maxM;
+}
+
+int main()
+{
+    int vect[5] = { 10, 17, -10, 54, 61 };
+    int matr[5][2] = { {17, 0}, {48, 24}, {100, 446}, {-16, 101}, {555, 32} };
+    Vector* vector = new Vector(vect);
+    Matrix* matrix = new Matrix(matr);
+    cout << Func(*vector, *matrix);
+}
